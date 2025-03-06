bc5d51db5e53118dc72097c030d61a2d55eb56a8
Samridhi Gumber <samridhigumber05@gmail.com>
2025-03-06 19:21:45 +0530

    SimpleCalc.c

---
+++ SimpleCalc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d", &a, &b);
+    scanf(" %c", &c);
+    if(c == '+'){
+        printf("%d", a+b);
+    }
+    else if(c == '-'){
+        printf("%d", a-b);
+    }
+    else if(c == '*'){
+        printf("%d", a*b);
+    }
+    else if(c == '/'){
+        if(b != 0){
+        printf("%d", a/b);
+    }   
+        else{
+            printf("Error");
+        }
+    }
+    else{
+        printf("Error");
+    } 
+    return 0;
+}
