a14c7de35e3593f1d6a05179452de5561a255743
Samridhi Gumber <samridhigumber05@gmail.com>
2025-03-06 19:23:58 +0530

    Sum_of_Even.c

---
+++ Sum_of_Even.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n, i=2, sum=0;
+    scanf("%d", &n);
+    while(i <= n){
+        sum += i;
+        i += 2;
+    }
+    printf("%d", sum);
+    return 0;
+}
