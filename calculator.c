eb260e8deabdd0586c407e7cc5024985b908fb66
Samridhi Gumber <samridhigumber05@gmail.com>
2025-03-12 19:42:48 +0530

    calculator

---
+++ calculator.c
@@ -0,0 +1,12 @@
+//Calculator
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d", &a, &b); // 3 2
+    printf("sum: %d\n", a+b);  // 5
+    printf("Subtract: %d\n", a-b); //1
+    printf("Multiplication: %d\n", a*b); // 6
+    printf("Division: %d\n", a/b); //1
+    printf("Remainder: %d\n", a%b); //1
+
+}
