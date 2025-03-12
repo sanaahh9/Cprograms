4aa2979ed15f586874561d1d9b2f656a5a757b87
Samridhi Gumber <samridhigumber05@gmail.com>
2025-03-12 20:03:09 +0530

    smallest_number

---
+++ smallest_number.c
@@ -0,0 +1,17 @@
+//smallest number
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c); 
+    if(a  < b && a < c){
+        printf("%d is smaller",a);
+    }
+    else if(b < a && b  < c){
+        printf("%d is smaller", b);
+    }
+    else{
+        printf("%d is smaller", c);
+    }
+     
+    return 0;
+}
