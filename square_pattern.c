9daccfccc343e882d35f205a18bbf1724bb4d1d7
Samridhi Gumber <samridhigumber05@gmail.com>
2025-03-21 16:58:37 +0530

    square_pattern

---
+++ square_pattern.c
@@ -0,0 +1,14 @@
+
+int main()
+{
+    int n,m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    for(int i =1; i<= n; i++){
+        for(int j = 1; j <=m; j++){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
